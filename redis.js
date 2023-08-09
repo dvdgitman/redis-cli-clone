cc36c25a9d8bf158ce38dcd7b7b3bfda9148e6eb
dvdgitman <wereking@gmail.com>
2023-08-09 15:04:00 +0300

    cleaner code

--- redis.js
+++ redis.js
@@ -1,71 +1,72 @@
-// This is the main function responsible for executing the Redis-like commands.
 function executeCommand() {
   const commandInput = document.getElementById('cliInput').value.trim();
   document.getElementById('cliInput').value = '';
-  const parts = commandInput.split(' ');
-  const command = parts[0].toUpperCase();
-  const key = parts[1];
-  const value = parts[2];
-
-  if (command === 'SET' && (!key || !value)) {
-      displayOutput('Please provide both a key and a value for the SET command.');
-      return;
-  }
 
   if (!commandInput) {
       displayOutput('Please enter a command.');
       return;
   }
 
+  const { command, key, value } = extractCommandParts(commandInput);
+  if (!validateCommand(command, key, value)) {
+      return;
+  }
+
   switch (command) {
       case 'SET':
-          setKeyValue(key, value);
+          const isExist = isKeyExist(key);
+          
+          if (isExist && !confirm('This key already exists. Do you want to overwrite the value?')) {
+            return false;
+          }
+
+          localStorage.setItem(key, value);
+          displayOutput(`Key: ${key}, Value: ${value} saved successfully.`);
           break;
       case 'GET':
-          getValue(key);
+          const storedValue = localStorage.getItem(key);
+          displayOutput(`Key: ${key}, Value: ${storedValue || 'not found'}.`);
           break;
       case 'DEL':
-          deleteValue(key);
+          localStorage.removeItem(key);
+          displayOutput(`Key: ${key} deleted successfully.`);
           break;
       case 'EXIST':
-          checkExistence(key);
+          const exists = isKeyExist(key);
+          displayOutput(`Key: ${key} ${exists ? 'exists.' : 'does not exist.'}`);
           break;
       case 'KEYS':
-          getKeys();
+          const keys = Object.keys(localStorage);
+          displayOutput(`Keys: ${keys.join(', ')}`);
           break;
       default:
           displayOutput('Invalid command.');
   }
 }
 
-function setKeyValue(key, value) {
-  if (localStorage.getItem(key) !== null) {
-      if (!confirm('This key already exists. Do you want to overwrite the value?')) {
-          return;
-      }
+function isKeyExist(key) {
+  return localStorage.getItem(key) !== null;
+}
+
+function extractCommandParts(commandInput) {
+  const [command, ...args] = commandInput.split(' ');
+  const key = args[0];
+  const value = args[1];
+  return { command: command.toUpperCase(), key, value };
+}
+
+function validateCommand(command, key, value) {
+  if (command === 'SET' && (!key || !value)) {
+      displayOutput('Please provide both a key and a value for the SET command.');
+      return false;
   }
-  localStorage.setItem(key, value);
-  displayOutput(`Key: ${key}, Value: ${value} saved successfully.`);
-}
 
-function getValue(key) {
-  const value = localStorage.getItem(key);
-  displayOutput(`Key: ${key}, Value: ${value || 'not found'}.`);
-}
+  if (!key && command !== 'KEYS') {
+    displayOutput('Please provide a key for the command.');
+    return false;
+  }
 
-function deleteValue(key) {
-  localStorage.removeItem(key);
-  displayOutput(`Key: ${key} deleted successfully.`);
-}
-
-function checkExistence(key) {
-  const exists = localStorage.getItem(key) !== null;
-  displayOutput(`Key: ${key} ${exists ? 'exists.' : 'does not exist.'}`);
-}
-
-function getKeys() {
-  const keys = Object.keys(localStorage);
-  displayOutput(`Keys: ${keys.join(', ')}`);
+  return true;
 }
 
 function displayOutput(message) {
